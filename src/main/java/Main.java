5d3315ce6aa1560aa2da6af5aa4dc5f6b38facc5
Krzysztof Piekarczyk <krzys@piekarczyk.eu>
2020-06-18 00:05:02 +0200

    Test importing JSONs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,28 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.util.Iterator;
+
 public class Main {
+
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        Timespan t = new Timespan("03:15", "11:12");
-        System.out.println(t.getStartString() + " - " + t.getStart());
-        System.out.println(t.getEndString() + " - " + t.getEnd());
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("callendar2.json"));
+
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray meetings = (JSONArray) jsonObject.get("planned_meeting");
+
+            Iterator<JSONObject> iterator = meetings.iterator();
+            while (iterator.hasNext()) {
+                JSONObject current = iterator.next();
+                System.out.println(current.get("start") + " - " + current.get("end"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
